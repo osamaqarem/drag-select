a7347caacc310b6de7c8d726176b85f332394185
Osama Qarem <osamaqarem@gmail.com>
2024-11-17 16:05:36 +0100

    Initial impl. & example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,169 @@
-import { multiply } from '@osamaqarem/react-native-drag-select';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useDragSelect } from "@osamaqarem/react-native-drag-select"
+import {
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  useWindowDimensions,
+  View,
+  type TextInputProps,
+} from "react-native"
+import {
+  GestureDetector,
+  GestureHandlerRootView,
+  type SimultaneousGesture,
+} from "react-native-gesture-handler"
+import Animated, {
+  useAnimatedProps,
+  useAnimatedRef,
+} from "react-native-reanimated"
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context"
 
 export default function App() {
-  const [result, setResult] = useState<number | undefined>();
-
-  useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={styles.gestureHandlerRootView}>
+        <List />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
+  )
 }
 
+type Item = { id: string }
+
+const data: Array<Item> = Array.from({ length: 100 }, (_, i) => ({
+  id: (i + 1).toString(),
+}))
+
+function List() {
+  const { width: deviceWidth } = useWindowDimensions()
+  const { top: topInset, bottom: bottomInset } = useSafeAreaInsets()
+
+  const flatlist = useAnimatedRef<FlatList<Item>>()
+
+  const {
+    panGesture,
+    createListItemGesture,
+    scrollHandler,
+    onListLayout,
+    clearSelection,
+    selectedItems,
+  } = useDragSelect({
+    data,
+    columnCount: 1,
+    columnSeparatorWidth: 0,
+    rowSeparatorHeight: 0,
+    headerHeight: 0,
+    gestureConfig: { longPressTiming: 300 },
+    listRef: flatlist,
+    itemSize: { height: 100, width: deviceWidth },
+    safeArea: { topInset, bottomInset },
+    onItemPress: (item) => {
+      console.log("onItemPress", item.id)
+    },
+    onItemSelected: (item) => {
+      console.log("onItemSelected", item.id)
+    },
+    onItemDeselected: (item) => {
+      console.log("DESELECT", item.id)
+    },
+  })
+
+  const animatedProps = useAnimatedProps(() => {
+    const count = Object.keys(selectedItems.value).length.toString()
+    return {
+      text: count,
+    } as TextInputProps
+  })
+
+  return (
+    <>
+      <GestureDetector gesture={panGesture}>
+        <SafeAreaView style={styles.safeArea}>
+          <Animated.FlatList<Item>
+            style={styles.flatlist}
+            data={data}
+            renderItem={({ item }) => (
+              <Item item={item} createGesture={createListItemGesture} />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            ref={flatlist}
+            onLayout={onListLayout}
+            onScroll={scrollHandler}
+          />
+        </SafeAreaView>
+      </GestureDetector>
+
+      <Pressable
+        style={[styles.clearBtn, { top: topInset + 14 }]}
+        onPress={clearSelection}
+      >
+        <AnimatedTextInput
+          animatedProps={animatedProps}
+          defaultValue="0"
+          style={styles.clearBtnText}
+          editable={false}
+        />
+      </Pressable>
+    </>
+  )
+}
+
+function Item({
+  item,
+  createGesture,
+}: {
+  item: Item
+  createGesture: (item: Item) => SimultaneousGesture
+}) {
+  const gesture = createGesture(item)
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <View style={styles.item}>
+        <Text>{item.id}</Text>
+      </View>
+    </GestureDetector>
+  )
+}
+
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput)
+
 const styles = StyleSheet.create({
-  container: {
+  gestureHandlerRootView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  safeArea: {
+    flex: 1,
+    backgroundColor: "pink",
   },
-});
+  flatlist: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  item: {
+    height: 100,
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "green",
+  },
+  clearBtn: {
+    minWidth: 50,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "lightgreen",
+    position: "absolute",
+    left: 14,
+  },
+  clearBtnText: {
+    textAlign: "center",
+    pointerEvents: "none",
+  },
+})
---
+++ src/useDragSelect.tsx
@@ -0,0 +1,390 @@
+import { FlatList, type LayoutRectangle } from "react-native"
+import { Gesture } from "react-native-gesture-handler"
+import {
+  interpolate,
+  runOnJS,
+  scrollTo,
+  useAnimatedScrollHandler,
+  useDerivedValue,
+  useFrameCallback,
+  useSharedValue,
+  type AnimatedRef,
+} from "react-native-reanimated"
+
+type Item = { id: string }
+
+export interface Config {
+  data: Array<Item>
+  columnCount: number
+  rowSeparatorHeight: number
+  columnSeparatorWidth: number
+  headerHeight: number
+  onItemPress: (item: Item) => void
+  onItemSelected: (item: Item) => void
+  onItemDeselected: (item: Item) => void
+  listRef: AnimatedRef<FlatList>
+  gestureConfig: {
+    longPressTiming: number
+  }
+  safeArea: {
+    topInset: number
+    bottomInset: number
+  }
+  itemSize: {
+    width: number
+    height: number
+  }
+}
+
+function getRecordMap<T extends { id: string }>(
+  list: Array<T>
+): Record<string, T> {
+  "worklet"
+  let record: Record<string, T> = {}
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i]
+    if (item) {
+      record[item.id] = item
+    }
+  }
+  return record
+}
+
+export const useDragSelect = (config: Config) => {
+  const itemMap = useDerivedValue(() => getRecordMap(config.data))
+
+  const selectedItems = useSharedValue<Record<string, Item>>({})
+  const flatlistLayout = useSharedValue<LayoutRectangle | null>(null)
+
+  const panTransitionFromIndex = useSharedValue<number | null>(null)
+  const panEvent = useSharedValue({
+    y: null as number | null,
+    absoluteX: null as number | null,
+  })
+
+  const scrollContentHeight = useSharedValue(0)
+  const scrollOffset = useSharedValue(0)
+
+  const selectedAxisName = useSharedValue("")
+
+  const selectModeActive = useDerivedValue(
+    () => Object.keys(selectedItems.value).length > 0
+  )
+
+  const handleDragSelect = (e: { y: number; absoluteX: number }) => {
+    "worklet"
+    if (!selectModeActive.value || !flatlistLayout.value || !config.data) return
+
+    const {
+      columnCount,
+      columnSeparatorWidth,
+      data,
+      headerHeight,
+      itemSize,
+      rowSeparatorHeight,
+      safeArea,
+    } = config
+
+    const windowHeight = flatlistLayout.value.height
+
+    const cellHeight = itemSize.height + rowSeparatorHeight
+    const cellWidth = itemSize.width + columnSeparatorWidth
+
+    const numItemsYAxis = Math.ceil(windowHeight / cellHeight)
+    const numItemsXAxis = columnCount
+
+    const headerArea = headerHeight + safeArea.topInset
+    // account for top padding
+    const topOffset = Math.max(0, headerArea - scrollOffset.value)
+    const safeTopPanY = e.y - topOffset
+    if (safeTopPanY < 0) return // panning in top padding
+
+    // account for bottom padding
+    const safeScrollableWindow = Math.max(
+      scrollContentHeight.value - windowHeight - safeArea.bottomInset,
+      0
+    )
+    const safeAreaPanY =
+      scrollOffset.value >= safeScrollableWindow
+        ? Math.min(safeTopPanY, windowHeight - safeArea.bottomInset)
+        : safeTopPanY
+    if (safeTopPanY !== safeAreaPanY) return // panning in bottom padding
+
+    // account for a row item being cut-off when scroll offset not 0
+    const headerHidden = scrollOffset.value >= headerArea
+    const breakpointYoffset = headerHidden
+      ? cellHeight -
+        ((scrollOffset.value - safeArea.topInset - headerHeight) % cellHeight)
+      : 0
+
+    let breakpointsY: Array<number> = [0]
+    let breakpointsX: Array<number> = [0]
+    if (breakpointYoffset) breakpointsY.push(breakpointYoffset)
+
+    Array(numItemsYAxis)
+      .fill(0)
+      .forEach((_, index) => {
+        breakpointsY.push((index + 1) * cellHeight + breakpointYoffset)
+      })
+    Array(numItemsXAxis)
+      .fill(0)
+      .forEach((_, index) => {
+        breakpointsX.push((index + 1) * cellWidth + columnSeparatorWidth)
+      })
+
+    const getValueBounds = (
+      value: number,
+      list: Array<number>
+    ): [number, number] => {
+      let idx = 0
+      for (const breakpoint of list) {
+        if (value >= breakpoint) {
+          idx += 1
+        } else {
+          return [idx - 1, idx]
+        }
+      }
+      return [idx - 1, idx]
+    }
+
+    let [lowerBoundY, upperBoundY] = getValueBounds(safeAreaPanY, breakpointsY)
+    let [lowerBoundX, upperBoundX] = getValueBounds(e.absoluteX, breakpointsX)
+    const lowY = breakpointsY[lowerBoundY] ?? -1
+    const highY = breakpointsY[upperBoundY] ?? -1
+    const lowX = breakpointsX[lowerBoundX] ?? -1
+    const highX = breakpointsX[upperBoundX] ?? -1
+
+    const withinX = e.absoluteX >= lowX && e.absoluteX <= highX
+    const withinY = safeAreaPanY >= lowY && safeAreaPanY <= highY
+
+    if (withinY && withinX) {
+      const scrolledRows = headerHidden
+        ? Math.floor(Math.abs((headerArea - scrollOffset.value) / cellHeight))
+        : 0
+      const rowBeginsAtIndex = scrolledRows * columnCount
+
+      const getArrayIndexForDimensions = (
+        rowIndex: number,
+        colIndex: number
+      ) => {
+        const arraysStartAtZero = 1
+        return (
+          rowIndex * columnCount -
+          (columnCount - colIndex) +
+          rowBeginsAtIndex -
+          arraysStartAtZero
+        )
+      }
+
+      const itemIndex = getArrayIndexForDimensions(upperBoundY, upperBoundX)
+
+      const getItemFromState = (index: number) => {
+        const itemInState = data[index]
+        return itemInState ? itemMap.value[itemInState.id] : undefined
+      }
+
+      const item = getItemFromState(itemIndex)
+      if (!item) return
+
+      if (panTransitionFromIndex.value === null) {
+        panTransitionFromIndex.value = itemIndex
+      }
+
+      /**
+       * axis cell: the cell where the long-press starts.
+       * next cell: the cell being entered
+       * previous cell: the cell being left
+       *
+       * Logic:
+       * - when entering a cell
+       *    - select all cells between the axis cell and the next cell
+       *         - when the next cell row is before the axis row
+       *              - deselect all cells after the axis
+       *         - when the cell row is after the axis row
+       *              - deselect all cells before the axis
+       */
+      const toIndex = itemIndex
+      if (panTransitionFromIndex.value !== toIndex) {
+        const selectItemAtIndex = (
+          i: number,
+          mutateObj: Record<string, Item>
+        ) => {
+          const curr = getItemFromState(i)
+          if (curr) {
+            const existing = mutateObj[curr.id]
+            if (!existing) {
+              mutateObj[curr.id] = curr
+            }
+          }
+        }
+        const deselectItemAtIndex = (
+          i: number,
+          mutateObj: Record<string, Item>
+        ) => {
+          const curr = getItemFromState(i)
+          if (curr) {
+            const existing = mutateObj[curr.id]
+            if (existing && existing.id !== selectedAxisName.value) {
+              delete mutateObj[curr.id]
+            }
+          }
+        }
+
+        const fromIndex = panTransitionFromIndex.value
+
+        const axisItem = selectedItems.value[selectedAxisName.value]
+        const axisIndex = data.findIndex((d) => d.id === axisItem?.id)
+
+        const axisRow = Math.floor(axisIndex / config.columnCount) + 1
+        const toRow = Math.floor(itemIndex / config.columnCount) + 1
+
+        const afterAxisRow = toRow > axisRow
+        const isAxisRow = toRow === axisRow
+
+        const backwards = toIndex < fromIndex
+        const forwards = toIndex > fromIndex
+
+        let nextSelectedItemsState = { ...selectedItems.value }
+
+        if (axisRow) {
+          if (forwards) {
+            for (let i = fromIndex; i < toIndex; i++) {
+              deselectItemAtIndex(i, nextSelectedItemsState)
+            }
+          } else if (backwards) {
+            for (let i = fromIndex; i > toIndex; i--) {
+              deselectItemAtIndex(i, nextSelectedItemsState)
+            }
+          }
+        }
+
+        if (afterAxisRow || (isAxisRow && forwards)) {
+          for (let i = axisIndex; i <= toIndex; i++) {
+            selectItemAtIndex(i, nextSelectedItemsState)
+          }
+        } else if (!afterAxisRow || (isAxisRow && backwards)) {
+          for (let i = axisIndex; i >= toIndex; i--) {
+            selectItemAtIndex(i, nextSelectedItemsState)
+          }
+        }
+        selectedItems.value = nextSelectedItemsState
+      }
+      panTransitionFromIndex.value = toIndex
+    }
+  }
+
+  const { setActive: setFrameCbActive } = useFrameCallback(() => {
+    const { absoluteX, y } = panEvent.value
+    if (
+      typeof absoluteX !== "number" ||
+      typeof y !== "number" ||
+      !selectModeActive.value ||
+      !flatlistLayout.value
+    ) {
+      return
+    }
+    const windowHeight = flatlistLayout.value.height
+    const bottomThreshold = windowHeight * 0.85
+    const topThreshold = windowHeight * 0.15
+
+    handleDragSelect({ absoluteX, y })
+
+    if (y > bottomThreshold) {
+      const inputRange = [bottomThreshold, windowHeight]
+      const outputRange = [0, 8]
+      const result = interpolate(y, inputRange, outputRange)
+      const offset = scrollOffset.value + result
+      scrollTo(config.listRef, 0, offset, false)
+    } else if (scrollOffset.value > 0 && y < topThreshold) {
+      const inputRange = [topThreshold, 0]
+      const outputRange = [0, 8]
+      const result = interpolate(y, inputRange, outputRange)
+      const offset = scrollOffset.value - result
+      scrollTo(config.listRef, 0, offset, false)
+    }
+  }, false)
+
+  const longPressOnStart = (id: string) => {
+    "worklet"
+    const longPressed = itemMap.value[id]
+    if (!longPressed) return
+    if (selectedItems.value[longPressed.id]) return
+    const axis = { ...longPressed, isLongPressAxis: true }
+    selectedAxisName.value = axis.id
+    selectedItems.value = {
+      ...selectedItems.value,
+      [longPressed.id]: axis,
+    }
+    runOnJS(config.onItemSelected)(longPressed)
+  }
+
+  const tapOnStart = (id: string) => {
+    "worklet"
+    const tapped = itemMap.value[id]
+    if (!tapped) return
+    if (selectModeActive.value) {
+      const item = selectedItems.value[id]
+      if (item) {
+        delete selectedItems.value[id]
+        selectedItems.value = { ...selectedItems.value }
+        runOnJS(config.onItemDeselected)(tapped)
+      } else {
+        selectedItems.value = { ...selectedItems.value, [id]: tapped }
+        runOnJS(config.onItemSelected)(tapped)
+      }
+    } else {
+      runOnJS(config.onItemPress)(tapped)
+    }
+  }
+
+  const panGesture = Gesture.Pan()
+    .activateAfterLongPress(config.gestureConfig.longPressTiming)
+    .onStart(() => {
+      runOnJS(setFrameCbActive)(true)
+    })
+    .onUpdate((e) => {
+      panEvent.value.y = e.y
+      panEvent.value.absoluteX = e.absoluteX
+    })
+    .onEnd(() => {
+      panTransitionFromIndex.value = null
+      panEvent.value.y = null
+      panEvent.value.absoluteX = null
+      runOnJS(setFrameCbActive)(false)
+    })
+
+  const createListItemGesture = (item: Item) => {
+    const tapGesture = Gesture.Tap()
+      .maxDuration(config.gestureConfig.longPressTiming)
+      .onStart(() => tapOnStart(item.id))
+
+    const longPressGesture = Gesture.LongPress()
+      .minDuration(config.gestureConfig.longPressTiming)
+      .onStart(() => longPressOnStart(item.id))
+      .simultaneousWithExternalGesture(panGesture)
+
+    return Gesture.Simultaneous(tapGesture, longPressGesture)
+  }
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    scrollOffset.value = event.contentOffset.y
+    scrollContentHeight.value = event.contentSize.height
+  })
+
+  function clearSelection() {
+    selectedItems.value = {}
+  }
+
+  function onListLayout(e: { nativeEvent: { layout: LayoutRectangle } }) {
+    flatlistLayout.value = e.nativeEvent.layout
+  }
+
+  return {
+    panGesture,
+    createListItemGesture,
+    clearSelection,
+    selectedItems,
+    onListLayout,
+    scrollHandler,
+  }
+}
